fcbb3343f7c6d704773b45e4b1094e0f338b7059
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2024-11-09 12:04:25 +0530

    feat: setup basic routing for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
-export default App;
+export default function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</Router>
+	);
+}
