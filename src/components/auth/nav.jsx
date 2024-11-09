573d405c3bd17bcb17e085fb929d078505476a3a
deepanshu garg <greekof022@gmail.com>
2024-11-09 10:57:46 +0530

    nav file

--- src/components/auth/nav.jsx
+++ src/components/auth/nav.jsx
@@ -1,7 +1,16 @@
-import { Routes, Route,BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 function Nav() {
   return (
-    
+    <>
+    <nav>  
+    <Link to="/signin">sign in</Link>
+    <Link to="/createaccount">create account</Link>
+    </nav>
+    <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/createaccount" element={<CreateAccount />} />
+    </Routes>
+    </>
   );
 }
 
